008d946ba445704263718b08718ea2455ebd304c
JBTG <pochopienantek@gmail.com>
2022-08-23 00:07:49 +0200

    added discover element details page

--- lib/widgets/discover_element.dart
+++ lib/widgets/discover_element.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../data/data_provider.dart';
+import '../screens/Discover_element_details.dart';
 
 class DiscoverElement extends StatefulWidget {
   int id;
@@ -28,89 +29,105 @@ class _DiscoverElementState extends State<DiscoverElement> {
   Widget build(BuildContext context) {
     final data = Provider.of<DataProvider>(context, listen: false);
 
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20),
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.5,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 300,
-                child: Card(
-                    semanticContainer: true,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: PageView.builder(
-                      itemCount: widget.images.length,
-                      itemBuilder: (context, index) {
-                        return widget.images[index];
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DiscoverElementDetails(
+                id: widget.id,
+                name: widget.name,
+                opinion: widget.opinion,
+                price: widget.price,
+                images: widget.images,
+                isFav: widget.isFav),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 300,
+                  child: Card(
+                      semanticContainer: true,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: PageView.builder(
+                        itemCount: widget.images.length,
+                        itemBuilder: (context, index) {
+                          return widget.images[index];
+                        },
+                      )),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 275,
+                  right: 0,
+                  bottom: 225,
+                  child: IconButton(
+                      onPressed: () {
+                        widget.isFav = !widget.isFav;
+                        data.toggleFav(widget.id);
                       },
-                    )),
-              ),
-              Positioned(
-                top: 0,
-                left: 275,
-                right: 0,
-                bottom: 225,
-                child: IconButton(
-                    onPressed: () {
-                      widget.isFav = !widget.isFav;
-                      data.toggleFav(widget.id);
-                    },
-                    icon: widget.isFav
-                        ? Icon(
-                            Icons.favorite,
-                            color: Colors.white,
-                            size: 30,
-                          )
-                        : Icon(
-                            Icons.favorite_border,
-                            color: Colors.white,
-                            size: 30,
-                          )),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                widget.name,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star_rounded,
-                    color: Colors.black,
-                    size: 18,
-                  ),
-                  Text(widget.opinion.toString())
-                ],
-              ),
-            ],
-          ),
-          Text(
-            'Gospodarz profesjonalny',
-            style: TextStyle(color: Colors.grey),
-          ),
-          Text(
-            '22-29 paź',
-            style: TextStyle(color: Colors.grey),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text('${widget.price} noc'),
-        ],
+                      icon: widget.isFav
+                          ? const Icon(
+                              Icons.favorite,
+                              color: Colors.white,
+                              size: 30,
+                            )
+                          : const Icon(
+                              Icons.favorite_border,
+                              color: Colors.white,
+                              size: 30,
+                            )),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.black,
+                      size: 18,
+                    ),
+                    Text(widget.opinion.toString())
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              'Gospodarz profesjonalny',
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text(
+              '22-29 paź',
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('${widget.price} noc'),
+          ],
+        ),
       ),
     );
   }
